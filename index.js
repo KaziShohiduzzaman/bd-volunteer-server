8be2a53a59cba6cfd95f384cd5f60969b4c3949e
KaziShohiduzzaman <kazishohiduzzaman786@gmail.com>
2021-10-28 03:27:25 +0600

    Database Connect

--- index.js
+++ index.js
@@ -1,9 +1,34 @@
-const express = require('express')
-const app = express()
-const port = 5000
+const express = require('express');
+const { MongoClient } = require('mongodb');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+const port = 5000;
+
+app.use(cors());
+app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.9zo1c.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+async function run() {
+    try {
+        await client.connect()
+        const database = client.db("bdVolunteer");
+        const volunteerCollection = database.collection("volunteer");
+
+
+
+    }
+    finally {
+        // await client.close()
+    }
+}
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
-    res.send('Volunteer server start')
+    res.send('Hello World!')
 })
 
 app.listen(port, () => {
